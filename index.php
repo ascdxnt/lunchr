22769697f2428eef15723b4c007713d60ccacda3
ecx <121473165+ecx2f@users.noreply.github.com>
2026-03-13 13:41:28 -0600

    refactor: rewrite core routing from spanish to english

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php
-require_once './Core/RutaFija.php';
-require_once './Core/Rutas.php';
+require_once './Core/DefaultRoute.php';
+require_once './Core/Router.php';
 
-require_once "./Controlador/IndexControlador.php";
+require_once "./Controller/IndexController.php";
 
-$ruta = new Rutas();
+$router = new Router();
 $dir = null;
 if (isset($_GET['dir'])) $dir = $_GET['dir'];
 
-if (isset($_GET['controlador'])) {
-  $controlador = $ruta->CargarControlador($_GET['controlador'], $dir);
-  if (isset($_GET['accion'])) {
-    if (isset($_GET['id'])) {
-      $ruta->CargarAccion($controlador, $_GET['accion'], $_GET['id']);
-    } else {
-      $ruta->CargarAccion($controlador, $_GET['accion']);
-    }
-  } else {
-    $ruta->CargarAccion($controlador, ACCION_PRINCIPAL);
-  }
+if (isset($_GET['controller'])) {
+	$controller = $router->LoadController($_GET['controller'], $dir);
+	if (isset($_GET['action'])) {
+		if (isset($_GET['id'])) {
+			$router->LoadAction($controller, $_GET['action'], $_GET['id']);
+		} else {
+			$router->LoadAction($controller, $_GET['action']);
+		}
+	} else {
+		$router->LoadAction($controller, DEFAULT_ACTION);
+	}
 } else {
-  $controlador = $ruta->CargarControlador(CONTROLADOR_PRINCIPAL, null);
-  $accionTmp = ACCION_PRINCIPAL;
-  $controlador->$accionTmp();
+	$controller = $router->LoadController(DEFAULT_CONTROLLER, null);
+	$defaultAction = DEFAULT_ACTION;
+	$controller->$defaultAction();
 }
